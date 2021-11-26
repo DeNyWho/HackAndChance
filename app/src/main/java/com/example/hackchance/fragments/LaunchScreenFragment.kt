61e4bac3cf7d1c9f71e32500babf1961710a40d1
Denyssdss <denis.akhunov00@mail.ru>
2021-11-26 22:05:04 +0500

    Some updates

--- app/src/main/java/com/example/hackchance/fragments/LaunchScreenFragment.kt
+++ app/src/main/java/com/example/hackchance/fragments/LaunchScreenFragment.kt
@@ -3,11 +3,8 @@ package com.example.hackchance.fragments
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.core.os.postDelayed
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.hackchance.R
 
